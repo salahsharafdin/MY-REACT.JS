2b32b0ed44f3cc746b87d38d724bc87dd293053d
salahsharafdin <salahsharafdin@gmail.com>
2026-03-15 02:58:23 +0300

    i complete the frist asiment

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "./componenst/Header";
+import Footer from "./componenst/Footer";
+import Hero from "./componenst/hero";
+import About from "./componenst/about";
+
+export const App = () => {
+    return (
+        <>
+        <Header />
+        <Hero />
+        <About />
+        <Footer />
+        </>
+    )
+}
+export default App;
